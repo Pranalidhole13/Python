ded0ebb76e06aa17879222623f91e5d208edfbc9
pranalidhole13 <pranalidhole63@gmail.com>
2024-03-02 23:25:02 +0530

    commit

---
+++ Basics/list.py
@@ -0,0 +1,12 @@
+#List append
+
+movies=[]
+mov1=input("Enter first movie name: ")
+mov2=input("Enter second movie name: ")
+mov3=input("Enter third movie name: ")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
+print(type(movies))
---
+++ Basics/stringlen.py
@@ -0,0 +1,9 @@
+#WAP to find the length of a string
+
+name=input("First name is: ")
+print(len(name))
+
+#WAP to find occurence
+
+label="$I $am $coder"
+print(label.count("$"))
