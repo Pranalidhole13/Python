f5b91311e1b7208b9340e183e2411e5e3c196a84
pranalidhole13 <pranalidhole63@gmail.com>
2024-03-05 17:21:52 +0530

    commit

---
+++ Basics/forLoop.py
@@ -0,0 +1,12 @@
+#Used for sequential traversal
+veggies = ["potato","Tomato","Brinjal"]
+tup = (1,2,3,4,5,6,7,8,9,10)
+
+j=1
+i=1
+for i in veggies:
+    print(i)
+
+for j in tup:
+    print(j)
+    
