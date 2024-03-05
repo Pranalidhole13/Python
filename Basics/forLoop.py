c60383e5b83b163657329594bffe8668027982ec
pranalidhole13 <pranalidhole63@gmail.com>
2024-03-05 18:29:48 +0530

    commit

--- Basics/forLoop.py
+++ Basics/forLoop.py
@@ -3,7 +3,7 @@ veggies = ["potato","Tomato","Brinjal"]
 tup = (1,2,3,4,5,6,7,8,9,10)
 
 j=1
-i=1
+# i=1
 for i in veggies:
     print(i)
 
---
+++ Basics/rangeFunction.py
@@ -0,0 +1,13 @@
+#range is sequence of numbers
+#Starting from 0 
+#Increments by 1 by default
+
+# seq=range(5) 
+for el in range(5):  #stop
+    print(el)
+
+for el in range(2,5):  #start,stop
+    print(el)
+
+for el in range(1,5,2):  #start,stop,increment
+    print(el)
